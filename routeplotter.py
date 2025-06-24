77fbe4d207609f0270c5055c5bf2620836365f16
mattyershov <mattyershov@gmail.com>
2025-06-24 17:16:25 -0400

    Added VMG optimization feature

--- routeplotter.py
+++ routeplotter.py
@@ -3,12 +3,29 @@ from gpsdclient import GPSDClient
 from geopy.distance import geodesic
 from geopy import Point
 
+sunfishpolar = { #twa : percentage of wind speed
+    0 : 0,
+   45 : 0.5,
+   50 : 0.75,
+   60 : 0.8,
+   90 : 0.85,
+   120 : 0.9,
+   150 : 1,
+   180 : 0.9,
+   210 : 0.8,
+   240 : 0.7,
+   270 : 0.65,
+   300 : 0.6,
+   310 : 0.5,
+   315 : 0.45
+}
+
 with GPSDClient() as client:
     for result in client.dict_stream(convert_datetime=True, filter=["TPV"]):
         lat =  result.get("lat", "n/a")
         lon = result.get("lon", "n/a")
-        trk = result.get("track", "n/a")
-        spd = (result.get("speed", "n/a")) * 0.51445
+        trk = int(result.get("track", "n/a"))
+        spd = int((result.get("speed", "n/a")) * 0.51445)
         if lat and lon:
             break
 
@@ -20,8 +37,24 @@ with GPSDClient() as client:
 
     print(f"{destination.latitude}, {destination.longitude}")
 
-    twa = 15
+    
     def calcvmg(bspd, twa):
-        twa_r = twa * (math.pi / 180)
-        vmg = bspd * math.cos(twa_r)
+        twarad = twa * (math.pi / 180)
+        vmg = bspd * math.cos(twarad)
         return vmg
+
+    def optimizevmg(twa):
+        if 0 <= twa <=90:
+            twalimit = 90
+        if 90 < twa <= 180:
+            twalimit = 90
+        if 270 <= twa < 0:
+            twalimit = 0
+        if 180 < twa < 270:
+            twalimit = 270
+        maxvmg = [0, 0] # [twa, vmg]
+        for i in range(twalimit - 90, twalimit):
+           vmgattempt = -0.131+0.0229(float(i))-.000168(float(i))^2+.000000499(float(i))^3-.000000000566(float(i))^4 # line of best fit, use Google Sheets to find one when you have the actual polar
+           if vmgattempt > maxvmg[1]:
+                maxvmg = [i, vmgattempt]
+        return maxvmg
